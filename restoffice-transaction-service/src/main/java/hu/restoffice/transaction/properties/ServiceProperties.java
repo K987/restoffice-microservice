0a198aee0bd577ae695de845c8385e0ccfc8ad39
Kálmán Kostenszky <k.kostenszky@gmail.com>
2018-04-29 11:35:14 +0200

    MOD: adding refreshscope

--- restoffice-transaction-service/src/main/java/hu/restoffice/transaction/properties/ServiceProperties.java
+++ restoffice-transaction-service/src/main/java/hu/restoffice/transaction/properties/ServiceProperties.java
@@ -1,12 +1,14 @@
 package hu.restoffice.transaction.properties;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
  *
  */
 @Component
+@RefreshScope
 public class ServiceProperties {
 
     @Value("${spring.application.name}")
