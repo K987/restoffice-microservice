972e30d7f84cbd24f02c2e4bd84a9c92b548cb99
Kálmán Kostenszky <k.kostenszky@gmail.com>
2018-04-29 11:22:38 +0200

    ADD: config properties component

--- restoffice-transaction-service/src/main/java/hu/restoffice/transaction/controller/TransactionRestController.java
+++ restoffice-transaction-service/src/main/java/hu/restoffice/transaction/controller/TransactionRestController.java
@@ -2,11 +2,13 @@ package hu.restoffice.transaction.controller;
 
 import java.security.Principal;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import hu.restoffice.transaction.properties.ServiceProperties;
 
 /**
  *
@@ -15,12 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class TransactionRestController {
 
-    @Value("${message}")
-    private String message;
+    @Autowired
+    private ServiceProperties properties;
 
     @RequestMapping("/message")
     public String message(final Principal principal) {
-        return message + " " + principal.getName();
+        return properties.getMassage() + " " + principal.getName();
     }
 
     @RequestMapping(path = "/message", method = RequestMethod.POST)
