779ca1e6daa363629caab4e682b5b46441c20898
Kálmán Kostenszky <k.kostenszky@gmail.com>
2018-05-10 23:33:13 +0200

    ADD: common lib

--- restoffice-dailyclose-service/src/main/java/hu/restoffice/dailyclose/RestofficeDailycloseServiceApplication.java
+++ restoffice-dailyclose-service/src/main/java/hu/restoffice/dailyclose/RestofficeDailycloseServiceApplication.java
@@ -3,12 +3,11 @@ package hu.restoffice.dailyclose;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-// @ComponentScan("hu.restoffice")
+@ComponentScan("hu.restoffice")
 public class RestofficeDailycloseServiceApplication {
 
     public static void main(final String[] args) {
--- restoffice-dailyclose-service/src/main/java/hu/restoffice/dailyclose/configuration/ResourceServerConfigurationProfileNotDev.java
+++ restoffice-dailyclose-service/src/main/java/hu/restoffice/dailyclose/configuration/ResourceServerConfigurationProfileNotDev.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -19,9 +21,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
  *
  */
 @Configuration
-// @Profile("test")
+@Profile("test")
 @EnableResourceServer
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfigurationProfileNotDev extends ResourceServerConfigurerAdapter {
 
     private static final Logger log = LogManager.getLogger();
